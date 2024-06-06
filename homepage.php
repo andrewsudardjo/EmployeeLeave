275a4ea075030c3e93c1e2590b808f0457bcc114
charlietjitra <86349013+charlietjitra@users.noreply.github.com>
2024-06-06 18:35:58 +0800

    Update homepage.php

--- homepage.php
+++ homepage.php
@@ -1,7 +1,40 @@
-
 <?php
-session_start();
+  session_start();
+  include ("dbconnect.php");
+  if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+      $username = $_POST['username'];
+      $password = $_POST['password'];
+
+      
+          // Prepare and execute the query to fetch the user
+      $stmt = $pdo->prepare('SELECT * FROM user WHERE username = :username');
+      $stmt->execute(['username' => $username]);
+
+          // Fetch the user
+      $user = $stmt->fetch(PDO::FETCH_ASSOC);
+      if ($user) {	
+        if (password_verify($password, $user['password'])) {
+          $_SESSION['user_id'] = $user['id'];
+          $_SESSION['username'] = $user['username'];
+          $_SESSION['role'] = $user['role'];
+
+          if($user['role'] == 'admin'){
+            header('Location: admin.php');
+          }else{
+            header('Location: apply-leave.php');
+          }
+          exit();
+
+          }else {
+          echo "<p style='color: red;'>Password verification failed.</p>";
+          }			
+    }else{
+    echo "<p style='color: red;'>username not found</p>";
+    }     
+  }
 ?>
+
+
 
 
 <!DOCTYPE html>
@@ -37,54 +70,4 @@ session_start();
 
   </body>
 </html>
-
-<?php
-    session_start();
-    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-        // Database configuration
-        $dsn = 'mysql:host=localhost;dbname=mydb';
-        $db_user = 'myapp';
-        $db_password = '1234';
-
-        // Get form data
-        $username = $_POST['username'];
-        $password = $_POST['password'];
-
-        try {
-            // Connect to the database
-            $pdo = new PDO($dsn, $db_user, $db_password);
-            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-            // Prepare and execute the query to fetch the user
-            $stmt = $pdo->prepare('SELECT * FROM user WHERE username = :username');
-            $stmt->execute(['username' => $username]);
-
-            // Fetch the user
-            $user = $stmt->fetch(PDO::FETCH_ASSOC);
-            if ($user) {	
-                if (password_verify($password, $user['password'])) {
-       			$_SESSION['user_id'] = $user['id'];
-			$_SESSION['username'] = $user['username'];
-                        $_SESSION['role'] = $user['role'];
-
-			if($user['role'] == 'admin'){
-				header('Location: admin.php');
-			}else{
-				header('Location: apply-leave.php');
-			}
-			exit();
-			
-    	    	}else {
-       			 echo "<p style='color: red;'>Password verification failed.</p>";
-    	    	}			
-	   }else{
-		echo "<p style='color: red;'>username not found</p>";
-    	   }
-            
-
-        } catch (PDOException $e) {
-            die("Could not connect to the database: " . $e->getMessage());
-        }
-    }
-?>
 
