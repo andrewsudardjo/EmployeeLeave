4dee482c5253b996bd098393ce542e982d43b893
charlietjitra <86349013+charlietjitra@users.noreply.github.com>
2024-06-06 18:36:53 +0800

    Update list.php

--- list.php
+++ list.php
@@ -2,24 +2,12 @@
 This code is for showing the list of username only 
 */
 <?php
-session_start();
-
-$dsn = 'mysql:host=localhost;dbname=mydb';
-$db_user = 'myapp';
-$db_password = '1234';
-
-try{
-	$pdo = new PDO($dsn, $db_user, $db_password);
-	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	$stmt = $pdo->prepare('SELECT id, username FROM user');
+	session_start();
+	include "dbconnect.php";
+	$stmt = $pdo->prepare('SELECT id, username, password FROM user');
 	$stmt->execute();
 
 	$users = $stmt->fetchAll(PDO::FETCH_ASSOC);
-	echo "$users[0]";
-}catch(PDOException $e){
-	die("couldntconnect");
-
-}
 ?>
 
 <!DOCTYPE html>
