b110cbca1d6adffca2e82da8149b35a7046f5531
andrewsudardjo <sudardjoandrew123@gmail.com>
2024-06-08 20:35:52 +0800

    Update register.php

--- register.php
+++ register.php
@@ -1,42 +1,11 @@
-<?php
-    include "dbconnect.php";
-    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-       
-        // Get form data
-        $username = $_POST['username'];
-        $password = $_POST['password'];
-	    $hashed_password = password_hash($password, PASSWORD_BCRYPT);
-
-        // Check if the username already exists
-        $stmt = $pdo->prepare('SELECT * FROM user WHERE username = :username');
-        $stmt->execute(['username' => $username]);
-        $existingUser = $stmt->fetch();
-
-        if ($existingUser) {
-            echo "<p style='color: red;'>Username already exists. Please choose a different username.</p>";
-        } else {
-            // Insert new user into the database
-            $stmt = $pdo->prepare('INSERT INTO user (username, password) VALUES (:username, :password)');
-            $stmt->execute(['username' => $username, 'password' => $hashed_password]);
-
-            echo '<script>
-                    setTimeout(function() {
-                        window.location.href = "homepage.php";
-                    }, 2000);
-                </script>';
-            
-            echo '<p style="color: green;">Registration successful. Redirecting to homepage...</p>';
-        }
-        
-    }
-?>
-
 <!DOCTYPE html>
 <html>
 <head>
     <title>Register</title>
+	<link href="register.css" rel="stylesheet">
 </head>
 <body>
+	<div class="register-page">
     <h2>Register</h2>
     <form method="POST" action="">
         <label for="username">Username:</label>
@@ -45,5 +14,33 @@
         <input type="password" name="password" id="password" required><br>
         <button type="submit">Register</button>
     </form>
+<?php
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        // Database configuration
+        include "dbconnect.php";
+        // Get form data
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+	$hashed_password = password_hash($password, PASSWORD_BCRYPT);
+
+   
+
+            // Check if the username already exists
+            $stmt = $pdo->prepare('SELECT * FROM user WHERE username = :username');
+            $stmt->execute(['username' => $username]);
+            $existingUser = $stmt->fetch();
+
+            if ($existingUser) {
+                echo "<p style='color: red;'>Username already exists. Please choose a different username.</p>";
+            } else {
+                // Insert new user into the database
+                $stmt = $pdo->prepare('INSERT INTO user (username, password) VALUES (:username, :password)');
+                $stmt->execute(['username' => $username, 'password' => $hashed_password]);
+
+                echo "<p style='color: green;'>Registration successful! You can now login with your new account.</p>";
+            }
+        } 
+    
+    ?>
 </body>
 </html>
