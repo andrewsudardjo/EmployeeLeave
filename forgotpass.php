b77c816d1912aa8f460d026ddcfd8d4cbfff8232
andrewsudardjo <sudardjoandrew123@gmail.com>
2024-06-08 19:59:39 +0800

    Update forgotpass.php

--- forgotpass.php
+++ forgotpass.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <title>Forgot Password</title>
-	<link href="forgotpass.css" rel="stylesheet">
+    <link href="forgotpass.css" rel="stylesheet">
 </head>
 <body>
     <div class="forgot-password-page">
@@ -14,30 +14,26 @@
             <br>
             <button type="submit" name="change">Submit</button>
             <p>Back to <a href="homepage.php">login page?</a></p>
-</div>    
-</form>
+        </form>
+    </div>
+
     <?php
     session_start();
-    if($_SERVER['REQUEST_METHOD']=='POST') {
-        try {
-            $pdo = new PDO('mysql:host=localhost;dbname=mydb', 'myapp', '1234');
-            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        include 'dbconnect.php';
 
-            $username = $_POST['username'];
-            $newpassword = $_POST['newpass'];
+        $username = $_POST['username'];
+        $newpassword = $_POST['newpass'];
 
-            // Hash the new password before storing it in the database
-            $hashed_password = password_hash($newpassword, PASSWORD_DEFAULT);
+        // Hash the new password before storing it in the database
+        $hashed_password = password_hash($newpassword, PASSWORD_DEFAULT);
 
-            $con = "UPDATE user SET password=:newpassword WHERE username=:username";
-            $chngpwd1 = $pdo->prepare($con);
-            $chngpwd1->bindParam(':username', $username, PDO::PARAM_STR);
-            $chngpwd1->bindParam(':newpassword', $hashed_password, PDO::PARAM_STR);
-            $chngpwd1->execute();
-            echo "Your Password successfully changed";
-        } catch(PDOException $e) {
-            die("Could not connect to database: " . $e->getMessage());
-        }
+        $con = "UPDATE user SET password = :newpassword WHERE username = :username";
+        $chngpwd1 = $pdo->prepare($con);
+        $chngpwd1->bindParam(':username', $username, PDO::PARAM_STR);
+        $chngpwd1->bindParam(':newpassword', $hashed_password, PDO::PARAM_STR);
+        $chngpwd1->execute();
+        echo "Your password has been successfully changed";
     }
     ?>
 </body>
